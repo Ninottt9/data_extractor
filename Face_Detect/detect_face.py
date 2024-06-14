ce0ccedbb7e2fae9edb7441b162bb7179d109e8d
Ninottt9 <mkazhaev746@gmail.com>
2024-06-14 20:55:49 +0200

    only bigger face image extraction added

--- Face_Detect/detect_face.py
+++ Face_Detect/detect_face.py
@@ -7,32 +7,37 @@ import traceback
 FACE_CASCADE = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 def detect_faces(image_path, output_folder, display=True):
-    # Load the image
     image = cv2.imread(image_path)
     if image is None:
         raise ValueError(f"Unable to load image at {image_path}")
 
-    # Convert image to grayscale
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the image
-    faces = FACE_CASCADE.detectMultiScale(image_gray, scaleFactor=1.16, minNeighbors=5, minSize=(25, 25), flags=0)
+    try:
+        faces = FACE_CASCADE.detectMultiScale(image_gray, scaleFactor=1.16, minNeighbors=5, minSize=(25, 25), flags=0)
+    except cv2.error as e:
+        print(f"OpenCV Error: {e}")
+        return
 
-    # Iterate through detected faces
-    for i, (x, y, w, h) in enumerate(faces):
-        # Extract the face region
+    # Initialize variables to keep track of the largest face
+    max_area = 0
+    max_face = None
+
+    # Iterate through detected faces to find the largest one
+    for (x, y, w, h) in faces:
+        face_area = w * h
+        if face_area > max_area:
+            max_area = face_area
+            max_face = (x, y, w, h)
+
+    # If a face was found, extract and save it
+    if max_face is not None:
+        x, y, w, h = max_face
         sub_img = image[y-10:y+h+10, x-10:x+w+10]
-
-        # Generate output file name
-        output_file = os.path.join(output_folder, f"{os.path.splitext(os.path.basename(image_path))[0]}_extracted_img_{i}.png")
-
-        # Save the extracted face image
+        output_file = os.path.join(output_folder, f"{os.path.splitext(os.path.basename(image_path))[0]}_extracted_img.png")
         cv2.imwrite(output_file, sub_img)
-
-        # Draw rectangle around the face on the original image
         cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
-    # Display the image with rectangles around detected faces
     if display:
         cv2.imshow("Faces Found", image)
         cv2.waitKey(0)
